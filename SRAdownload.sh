2eebd28b3e4ce835e4b53bb3543255ab2ec4e364
Nadia Baig <51199820+nadiabaig@users.noreply.github.com>
2023-08-02 07:39:26 +0000

    Create SRAdownload.sh

---
+++ SRAdownload.sh
@@ -0,0 +1,22 @@
+nadia baig <nadiabeg.comsats@gmail.com>
+	
+07:38 (2 hours ago)
+	
+to me
+
+#!/bin/bash
+
+# Replace 'accessions.txt' with the actual name of your file containing the list of SRA accessions
+file="accessions.txt"
+
+# Check if the file exists
+if [ ! -f "$file" ]; then
+    echo "Error: File '$file' not found."
+    exit 1
+fi
+
+# Loop through each line in the file and download the SRA data
+while IFS= read -r accession; do
+    echo "Downloading accession: $accession"
+    fastq-dump "$accession" # Replace 'fastq-dump' with the appropriate SRA Toolkit command for downloading data
+done < "$file"
