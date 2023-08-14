28135ab693a642ba9a286d827a93860b5816b195
Nadia Baig <51199820+nadiabaig@users.noreply.github.com>
2023-08-14 08:51:00 +0200

    Update SRAdownload.sh

--- SRAdownload.sh
+++ SRAdownload.sh
@@ -1,18 +1,27 @@
 
 
+
 #!/bin/bash
 
-# Replace 'accessions.txt' with the actual name of your file containing the list of SRA accessions
-file="accessions.txt"
+# Replace 'fastq-dump' with the appropriate SRA Toolkit command for downloading data
+download_command="/mnt/d/Softwares/sratoolkit.3.0.6-ubuntu64/bin/fastq-dump"
+path="/mnt/server/Rawdata/WGS"
+file="wg.txt"
 
-# Check if the file exists
-if [ ! -f "$file" ]; then
-    echo "Error: File '$file' not found."
-    exit 1
-fi
 
-# Loop through each line in the file and download the SRA data
-while IFS= read -r accession; do
+# Function to download an accession
+download_accession() {
+    accession="$1"
     echo "Downloading accession: $accession"
-    fastq-dump "$accession" # Replace 'fastq-dump' with the appropriate SRA Toolkit command for downloading data
+    $download_command --split-files "$accession" --outdir "$path"
+}
+
+# Read the accessions from the file and call the download function in the background
+while IFS= read -r accession; do
+    download_accession "$accession" &
 done < "$file"
+
+# Wait for all background jobs to complete
+wait
+
+echo "Download complete!"
